81fa6dcc9bdd3d426a50fb8dbef0c1c8cf14a874
Daniel Alberto Osuna <daniel.osuna@globant.com>
2016-12-16 16:28:06 -0300

    first commit

---
+++ FindCrossRefs/DBSet.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+
+namespace FindCrossRefs
+{
+    public class DBSet : IDisposable
+    {
+        public SQLiteConnection Connection { get; private set; }
+
+        public int functId;
+        public int refId;
+        public object functObj = new object();
+        public object refObj  = new object();
+
+        public DBSet(string dbName)
+        {
+            Connection = new SQLiteConnection("Data Source=" + dbName + ";version=3;");
+            Connection.Open();
+            string sql = "CREATE TABLE IF NOT EXISTS Methods (Id INTEGER PRIMARY KEY, NamespaceName VARCHAR(200), TypeName VARCHAR(200), Name VARCHAR(400))";
+
+            using (SQLiteCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            sql = "CREATE TABLE IF NOT EXISTS Refs (Id INTEGER PRIMARY KEY, CallerID INTEGER, NamespaceName VARCHAR(200), TypeName VARCHAR(200), Name VARCHAR(400))";
+            using (SQLiteCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            sql = "DELETE FROM Methods";
+            using (SQLiteCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+                functId = 0;
+
+            }
+            sql = "DELETE FROM Refs";
+            using (SQLiteCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+                refId = 0;
+            }
+
+        }
+
+        public int WriteFunction(string namespaceName, string typeName, string function)
+        {
+
+            lock (functObj)
+            {
+                functId++;
+            }
+            string sql = string.Format("INSERT INTO Methods( Id, NamespaceName, TypeName, Name) VALUES ({0},'{1}','{2}','{3}')", functId, namespaceName, typeName, function);
+            using (SQLiteCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            return functId;
+        }
+        public int WriteReference(int callerId, string namespaceName, string typeName, string functionName)
+        {
+
+            lock (refObj)
+            {
+                refId++;
+            }
+            string sql = string.Format("INSERT INTO Refs( Id, CallerId, NamespaceName, TypeName,  Name) VALUES ({0},{1},'{2}','{3}','{4}')", refId, callerId, namespaceName, typeName, functionName);
+            using (SQLiteCommand cmd = Connection.CreateCommand())
+            {
+                cmd.CommandText = sql;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            return functId;
+        }
+
+        public void Dispose()
+        {
+            Connection.Dispose();
+        }
+    }
+}
---
+++ FindCrossRefs/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Mono.Reflection;
+
+namespace FindCrossRefs
+{
+    class Program
+    {
+
+        private static Dictionary<string, string> functionIndex;
+        private static string dbName;
+        private static DBSet db;
+
+        static void Main(string[] args)
+        {
+            string typeToCheck = args[0];
+            Console.WriteLine("Process started");
+            List<string> assemblyList = ReadList(args[0]);
+            db = new DBSet(dbName);
+
+            ReadAssemblies(assemblyList);
+
+            db.Dispose();
+            //Type type         = assembly.GetType(args[1]); ;
+            //Check(type, args[2]);
+            Console.WriteLine("Process finished. Press [Enter] to continue...");
+            Console.ReadLine();
+        }
+
+        static void Check(Type checkType, string methodName)
+        {
+            
+            MethodBase methodBase = checkType.GetMethod(methodName, new Type[] { typeof(Int32)} );
+            var instructions = MethodBodyReader.GetInstructions(methodBase);
+
+            foreach (Instruction instruction in instructions)
+            {
+                MethodInfo methodInfo = instruction.Operand as MethodInfo;
+
+                if (methodInfo != null)
+                {
+                    Type type = methodInfo.DeclaringType;
+                    ParameterInfo[] parameters = methodInfo.GetParameters();
+
+                    Console.WriteLine("{0}.{1}({2});",
+                        type.FullName,
+                        methodInfo.Name,
+                        String.Join(", ", parameters.Select(p => p.ParameterType.FullName + " " + p.Name).ToArray())
+                    );
+                }
+            }
+        }
+
+        static List<string> ReadList(string scriptName)
+        {
+            List<string> list = new List<string>();
+
+            List<string> lines =  File.ReadLines(scriptName).ToList();
+
+            lines.ForEach(l => {
+                if (l.StartsWith("in:"))
+                {
+                    list.Add(l.Substring(3));
+                }
+                else if (l.StartsWith("out:"))
+                {
+                    dbName = l.Substring(4);
+                }
+            });
+
+            return list;
+        }
+
+        static void ReadAssemblies(List<string> assemblyList)
+        {
+            assemblyList.ForEach(a =>
+            {
+                Console.WriteLine("Reading {0}", a);
+                Assembly assembly = Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, a));
+                List<Type> types  =  assembly.GetTypes().ToList();
+                types.ForEach(t => {
+                    List<MethodInfo> methods = t.GetMethods().ToList();
+                    methods.ForEach(m => {
+                        MethodBase methodBase = m;
+
+                        ParameterInfo[] parameters = m.GetParameters();
+                        string functionName = string.Format("{0}({1})",
+                            m.Name,
+                            String.Join(", ", parameters.Select(p => p.ParameterType.FullName).ToArray())
+                        );
+                        int functId = db.WriteFunction( t.Namespace, t.Name, functionName);
+                        
+                        var instructions = MethodBodyReader.GetInstructions(methodBase);
+
+                        foreach (Instruction instruction in instructions)
+                        {
+                            MethodInfo methodInfo = instruction.Operand as MethodInfo;
+
+                            if (methodInfo != null)
+                            {
+                                Type type = methodInfo.DeclaringType;
+                                ParameterInfo[]  instParameters = methodInfo.GetParameters();
+
+                                string instName = string.Format( "{0}({1})",
+                                    methodInfo.Name,
+                                    String.Join(", ", instParameters.Select(p => p.ParameterType.FullName ).ToArray())
+                                );
+
+                                db.WriteReference(functId, type.Namespace, type.Name, instName);
+                            }
+                        }
+                    });
+                });
+            });
+        }
+    }
+}
